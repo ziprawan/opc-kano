866d42e516e33d087dfe27feccfd2b0a8bbd40ac
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2025-06-25 01:22:36 +0000

    handler: message: Add leaderboard handler function
    This function will handler .leaderboard and .lb command
    This command can only be used from group.
    
    This command will rank users from the group based on their game points.
    Bot will reply 5 biggest score and the sender rank and score.
    Text will also formatted as bold to mark which is the sender.
    
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

---
+++ internals/handlers/message/leaderboard.go
@@ -0,0 +1,105 @@
+package message
+
+import (
+	"database/sql"
+	"fmt"
+	"kano/internals/database"
+	"strings"
+)
+
+const LEADERBOARD_QUERY = `WITH
+	"ranked" AS (
+		SELECT
+			"c"."jid",
+			"c"."push_name",
+			"c"."custom_name",
+			"cs"."game_points",
+			ROW_NUMBER() OVER (
+				ORDER BY
+					"cs"."game_points" DESC
+			) AS "rank"
+		FROM
+			"participant" "p"
+			INNER JOIN "contact_settings" "cs" ON "cs"."id" = "p"."contact_id"
+			AND "cs"."game_points" != 0
+			INNER JOIN "contact" "c" ON "c"."id" = "cs"."id"
+		WHERE
+			"p"."group_id" = $1
+	),
+	"top5" AS (
+		SELECT
+			*
+		FROM
+			"ranked"
+		ORDER BY
+			"rank"
+		LIMIT
+			5
+	),
+	"sender" AS (
+		SELECT
+			*
+		FROM
+			"ranked"
+		WHERE
+			"jid" = $2
+	)
+SELECT
+	*
+FROM
+	"top5"
+UNION
+SELECT
+	*
+FROM
+	"sender"
+ORDER BY
+	"rank"`
+
+func LeaderboardHandler(ctx *MessageContext) {
+	if ctx.Instance.Group == nil {
+		ctx.Instance.Reply("Pakenya di grup aja ya bang", true)
+		return
+	}
+
+	db := database.GetDB()
+	rows, err := db.Query(LEADERBOARD_QUERY, ctx.Instance.Group.ID, ctx.Instance.Contact.JID.String())
+	if err != nil {
+		fmt.Println("leaderboard: Failed to create query:", err)
+		ctx.Instance.Reply("Terjadi kesalahan saat membuat kueri", true)
+		return
+	}
+	defer rows.Close()
+
+	msg := "*===== LEADERBOARD =====*\n\n"
+
+	for rows.Next() {
+		var jid string
+		var pushName, customName sql.NullString
+		var gamePoints, rank int
+		err := rows.Scan(&jid, &pushName, &customName, &gamePoints, &rank)
+		if err != nil {
+			fmt.Println("leaderboard: Failed to scan result:", err)
+			ctx.Instance.Reply("Terjadi kesalahan saat mengambil data", true)
+			return
+		}
+
+		name := strings.Split(jid, "@")[0]
+		if customName.Valid && len(customName.String) > 0 {
+			name = customName.String
+		} else if pushName.Valid && len(pushName.String) > 0 {
+			name = pushName.String
+		}
+
+		add := ""
+		if jid == ctx.Instance.Contact.JID.String() {
+			add = fmt.Sprintf("%d. *%s (%d points)*", rank, name, gamePoints)
+		} else {
+			add = fmt.Sprintf("%d. %s (%d points)", rank, name, gamePoints)
+		}
+
+		msg += add + "\n"
+	}
+
+	ctx.Instance.Reply(msg, true)
+}
