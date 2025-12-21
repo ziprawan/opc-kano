a27ded3e10b738559536883994c27236b3afad32
Aziz Ridhwan Pratama <azizpratama2196@gmail.com>
2025-12-21 19:46:59 +0700

    utils: bytes: Add more conversion
    Signed-off-by: Aziz Ridhwan Pratama <azizpratama2196@gmail.com>

--- internal/utils/numbers/bytes.go
+++ internal/utils/numbers/bytes.go
@@ -4,30 +4,58 @@ import (
 	"encoding/binary"
 )
 
-func ByteToUint32LSB(bytes []byte) int {
+func ByteToUint32LSB(bytes []byte) uint32 {
 	copied := make([]byte, 4)
 	for i := range len(bytes) {
 		copied[i] = bytes[i]
 	}
-	return int(binary.LittleEndian.Uint32(copied))
+	return binary.LittleEndian.Uint32(copied)
 }
 
-func Int32ToByteLSB(num int) []byte {
+func ByteToUint32MSB(bytes []byte) uint32 {
+	copied := make([]byte, 4)
+	for i := range len(bytes) {
+		copied[i] = bytes[i]
+	}
+	return binary.BigEndian.Uint32(copied)
+}
+
+func Uint32ToByteLSB(num uint) []byte {
 	b := make([]byte, 4)
 	binary.LittleEndian.PutUint32(b, uint32(num))
 	return b
 }
 
-func ByteToUint16LSB(bytes []byte) int {
+func Uint32ToByteMSB(num uint) []byte {
+	b := make([]byte, 4)
+	binary.BigEndian.PutUint32(b, uint32(num))
+	return b
+}
+
+func ByteToUint16LSB(bytes []byte) uint16 {
 	copied := make([]byte, 2)
 	for i := range len(bytes) {
 		copied[i] = bytes[i]
 	}
-	return int(binary.LittleEndian.Uint16(copied))
+	return binary.LittleEndian.Uint16(copied)
 }
 
-func Int16ToByteLSB(num int) []byte {
+func ByteToUint16MSB(bytes []byte) uint16 {
+	copied := make([]byte, 2)
+	for i := range len(bytes) {
+		copied[i] = bytes[i]
+	}
+	return binary.BigEndian.Uint16(copied)
+}
+
+func Uint16ToByteLSB(num uint) []byte {
 	b := make([]byte, 2)
 	binary.LittleEndian.PutUint16(b, uint16(num))
+	return b
+}
+
+func Uint16ToByteMSB(num uint) []byte {
+	b := make([]byte, 2)
+	binary.BigEndian.PutUint16(b, uint16(num))
 	return b
 }
